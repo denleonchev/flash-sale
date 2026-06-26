3112bcb1b4e87820f56a7657df15acdafc544580
den.leonchev@gmal.com <den.leonchev@gmail.com>
2026-06-26 20:25:46 +0000

    lint and format

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettierConfig from "eslint-config-prettier";
+
+export default tseslint.config(
+  {
+    ignores: [
+      "**/dist/**",
+      "**/.next/**",
+      "**/node_modules/**",
+      "**/generated/**", // Prisma-generated files
+    ],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierConfig,
+  {
+    rules: {
+      // Allow _-prefixed vars/params as intentionally unused
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+);
